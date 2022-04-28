7f4cf1517cb27d23341a906297fd4ef362d62039
GithinOommen <90901501+GithinOommen@users.noreply.github.com>
2022-04-28 14:09:38 +0530

    Update thresholdCheck.h

--- thresholdCheck.h
+++ thresholdCheck.h
@@ -2,4 +2,6 @@
 #define THRESHOLDCHECK_H
 extern int checkLowLimit(float input, float lowLimit, float highLimit, int* error_Code);
 extern int checkHighLimit(float input, float lowLimit, float highLimit, int* error_Code) ;
+extern int FailureAlertCheck(int Low, int High);
+extern int PrintDecision(int lowstatus, int highstatus);
 #endif
